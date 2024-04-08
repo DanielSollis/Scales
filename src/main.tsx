297722b90f73364a6da4bccc331f6c7aad6c6769
DanielSollis <daniel@rotational.io>
2024-04-08 00:27:09 -0700

    fixed import bug

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
 import FretBoard from "./components/FretBoard.tsx";
 import ScaleDropdown from "./components/ScaleDropdown.tsx";
 import Table from "react-bootstrap/Table";
@@ -7,7 +8,6 @@ import Table from "react-bootstrap/Table";
 const FretNums = function () {
   return (
     <Table bordered>
-      <style>{`.equal-width-table td {width: 1%;}`}</style>
       {[...Array(12)].map(function (_, index) {
         return <td className="text-center">{index}</td>;
       })}
